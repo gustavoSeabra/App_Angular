27924898dc8d28bc37476e3069126bccd5580659
gustavoSeabra <gustavo.henrique.ss@gmail.com>
2020-02-14 15:36:30 -0300

    Adicionando uma barra de navegação ao projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
+import { NavBarComponent } from './navBar/navBar.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      EventosComponent
+      EventosComponent,
+      NavBarComponent
    ],
    imports: [
       BrowserModule,
