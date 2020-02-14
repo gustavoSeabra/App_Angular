d1c4f2f5d68699a878d4e225787c00b0d3191c8d
gustavoSeabra <gustavo.henrique.ss@gmail.com>
2020-02-14 15:02:46 -0300

    Adicionando chamada de API, Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // Nome da minha tag
   selector: 'app-root',
+  // Nome do meu componente - Arquivo onde está o html
   templateUrl: './app.component.html',
+  // CSS do meu componente
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ProAgilApp';
+  title = 'ProAgil Eventos';
 }
---
+++ src/app/eventos/eventos.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.css']
+})
+export class EventosComponent implements OnInit {
+
+  eventos: any;
+  /*
+  = [
+    {
+      EventoId: 1,
+      Tema: 'Angular',
+      Local: 'Belo Horizonte'
+    },
+    {
+      EventoId: 2,
+      Tema: ',Net core',
+      Local: 'São Paulo'
+    },
+    {
+      EventoId: 3,
+      Tema: 'Angular e .Net Core',
+      Local: 'Rio de Janeiro'
+    },
+
+  ]
+  */
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getEventos();
+  }
+
+  getEventos(){
+    this.http.get('https://localhost:5001/api/values').subscribe(response => {
+        this.eventos = response;
+      }, error => {
+        console.log(error);
+      }
+    );
+  }
+
+}
