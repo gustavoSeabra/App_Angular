0a19161ee3cdcc9cf91a05862a4699f86b97be02
gustavo_seabra <gustavo.henrique.ss@gmail.com>
2020-03-14 00:26:22 -0300

    Montando o grid

--- src/app/eventos/eventos.component.ts
+++ src/app/eventos/eventos.component.ts
@@ -1,48 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-eventos',
-  templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.css']
+  selector: "app-eventos",
+  templateUrl: "./eventos.component.html",
+  styleUrls: ["./eventos.component.css"]
 })
 export class EventosComponent implements OnInit {
-
   eventos: any;
-  /*
-  = [
-    {
-      EventoId: 1,
-      Tema: 'Angular',
-      Local: 'Belo Horizonte'
-    },
-    {
-      EventoId: 2,
-      Tema: ',Net core',
-      Local: 'São Paulo'
-    },
-    {
-      EventoId: 3,
-      Tema: 'Angular e .Net Core',
-      Local: 'Rio de Janeiro'
-    },
+  imgLargura: number = 50;
+  imgAltura: number = 40;
 
-  ]
-  */
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.getEventos();
   }
 
   getEventos() {
-    this.http.get('https://localhost:5001/api/values').subscribe(response => {
+    this.http.get("https://localhost:5001/api/values").subscribe(
+      response => {
         this.eventos = response;
-      }, error => {
+      },
+      error => {
         console.log(error);
       }
     );
   }
 
+  mostrarImagens() {
+    alert("Deu bom!!");
+  }
 }
